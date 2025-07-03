c0c49c347338dacf356489d72bb555b98a12e093
mmarong-hw <marong.recruit@gmail.com>
2025-07-03 20:00:47 +0900

    update: make field type & defaultField

---
+++ src/types/field.ts
@@ -0,0 +1,31 @@
+interface CommonField {
+  label: string;
+  required: boolean;
+}
+
+export interface TextField extends CommonField {
+  type: "text";
+}
+
+export interface TextareaField extends CommonField {
+  type: "textarea";
+}
+
+export interface DateField extends CommonField {
+  type: "date";
+}
+
+export interface SelectField extends CommonField {
+  type: "select";
+  items: string[];
+}
+
+export interface CheckboxField extends CommonField {
+  type: "checkbox";
+}
+
+export type Field = TextField |
+  TextareaField |
+  DateField |
+  SelectField |
+  CheckboxField;
